b61fd8c1a03a400bcf119119238db9d036218529
Oscarlaviana1 <56448025+Oscarlaviana1@users.noreply.github.com>
2025-02-13 16:40:53 +0100

    cambiando texto por quijote y principe de bel air
    commit borrado linea rebase
    
    commit 2 nueva linea
    
    empezando el quijote
    
    añadiendo el principe de bel air

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -3,15 +3,10 @@ package demo;
 public class Demo1 {
 	
 	public static void main(String[] args) {
-		System.out.println("Linea 1");
-		System.out.println("Linea 2");
-		System.out.println("Linea 3");
+		System.out.println("En un lugar de la mancha de cuyo nombre no quiero acordarme...");
 		
-		System.out.println("cambios en main, con branch 1 creada");
-		
-		System.out.println("cambio simultaneo entre dos ramas. Naranja");
-		
-		System.out.println("cambio simultaneo con rebase. Armario");
+		System.out.println("Crecia y vivia sin hacer mucho caso a la policia");
+		System.out.println("Jugaba al basket, sin cansarme demasiado, mientras por las noches me sacaba el doctorado");
 	}
 
 }
